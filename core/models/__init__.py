f40f1d35dda4d0a6a95fb3233e5d56327abe9bff
yangxinpeng <363063705@qq.com>
2024-11-14 10:08:17 +0800

    init for model

---
+++ core/models/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :__init__.py.py
+# @Time      :2024/6/11 20:20
+# @Author    :Yangxinpeng
+# @Introduce :
+from core.models.backbones import *
+from core.models.TFSNet import *
